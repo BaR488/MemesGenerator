d10ae0b4e830b9611879d775b946a0a4e178021c
bar488 <boris_traktor@mail.ru>
2016-07-17 18:57:46 +0500

    Редактирование размера и положение текста. Убраны черные края.

--- app/src/main/java/com/example/boris/memesgenerator/Fragments/MakeLayoutFragment.java
+++ app/src/main/java/com/example/boris/memesgenerator/Fragments/MakeLayoutFragment.java
@@ -3,11 +3,13 @@ package com.example.boris.memesgenerator.Fragments;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +17,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 
+import com.example.boris.memesgenerator.Helpers.BitmapHelper;
 import com.example.boris.memesgenerator.R;
 
 import java.io.File;
@@ -66,13 +70,18 @@ public class MakeLayoutFragment extends Fragment {
         }else{
             RelativeLayout relativeLayout = (RelativeLayout) getView().findViewById(R.id.mem4ik);
             relativeLayout.setVisibility(View.GONE);
+
+            SeekBar seekBar = (SeekBar) getView().findViewById(R.id.seekSize);
+            seekBar.setVisibility(View.GONE);
+            SeekBar seekBar1 = (SeekBar) getView().findViewById(R.id.seekMargen);
+            seekBar1.setVisibility(View.GONE);
             Button button  = (Button) getView().findViewById(R.id.shareButton);
             button.setEnabled(false);
 
         }
     }
 
-    public void setupListeners(View view){
+    public void setupListeners(final View view){
         Button button = (Button) view.findViewById(R.id.shareButton);
         button.setOnClickListener(new View.OnClickListener()
         {
@@ -83,6 +92,8 @@ public class MakeLayoutFragment extends Fragment {
             }
         });
 
+
+
         EditText editText = (EditText) view.findViewById(R.id.findMeme);
         editText.setOnFocusChangeListener(new View.OnFocusChangeListener(){
             @Override
@@ -90,6 +101,71 @@ public class MakeLayoutFragment extends Fragment {
                 if (hasFocus){
                     findListener.onFind();
                 }
+            }
+        });
+
+        SeekBar seekBar = (SeekBar) view.findViewById(R.id.seekSize);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                EditText editTextTop = (EditText) view.findViewById(R.id.topText);
+                EditText editTextBot = (EditText) view.findViewById(R.id.bottomText);
+                editTextTop.setTextSize(TypedValue.COMPLEX_UNIT_PT, progress);
+                editTextBot.setTextSize(TypedValue.COMPLEX_UNIT_PT, progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        SeekBar seekBar1 = (SeekBar) view.findViewById(R.id.seekMargen);
+        seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                Resources r = getActivity().getResources();
+                int px = (int) TypedValue.applyDimension(
+                        TypedValue.COMPLEX_UNIT_DIP,
+                        progress,
+                        r.getDisplayMetrics()
+                );
+
+                EditText editTextTop = (EditText) view.findViewById(R.id.topText);
+                EditText editTextBot = (EditText) view.findViewById(R.id.bottomText);
+                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) editTextTop.getLayoutParams();
+                layoutParams.setMargins(
+                        layoutParams.leftMargin,
+                        px,
+                        layoutParams.rightMargin,
+                        layoutParams.bottomMargin);
+                editTextTop.setLayoutParams(layoutParams);
+
+                RelativeLayout.LayoutParams layoutParams1 = (RelativeLayout.LayoutParams) editTextBot.getLayoutParams();
+                layoutParams1.setMargins(
+                        layoutParams1.leftMargin,
+                        layoutParams1.topMargin,
+                        layoutParams.rightMargin,
+                        px);
+                editTextBot.setLayoutParams(layoutParams1);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
     }
@@ -107,21 +183,21 @@ public class MakeLayoutFragment extends Fragment {
         try {
             // image naming and path  to include sd card  appending name you choose for file
             String mPath = Environment.getExternalStoragePublicDirectory(
-                    Environment.DIRECTORY_PICTURES).toString() + "/" + nd + ".jpg";
+                    Environment.DIRECTORY_PICTURES).toString() + "/" + nd + ".png";
 
             // create bitmap screen capture
 
             View v1 = getView().findViewById(R.id.mem4ik);
             v1.requestFocus();
             v1.setDrawingCacheEnabled(true);
-            Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+            Bitmap bitmap = BitmapHelper.TrimBitmap(Bitmap.createBitmap(v1.getDrawingCache()));
             v1.setDrawingCacheEnabled(false);
 
             File imageFile = new File(mPath);
 
             FileOutputStream outputStream = new FileOutputStream(imageFile);
             int quality = 100;
-            imageSaved = bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+            imageSaved = bitmap.compress(Bitmap.CompressFormat.PNG, quality, outputStream);
             outputStream.flush();
             outputStream.close();
 
---
+++ app/src/main/java/com/example/boris/memesgenerator/Helpers/BitmapHelper.java
@@ -0,0 +1,82 @@
+package com.example.boris.memesgenerator.Helpers;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+
+/**
+ * Created by boris on 17.07.2016.
+ */
+public class BitmapHelper {
+    public static Bitmap TrimBitmap(Bitmap bmp) {
+        int imgHeight = bmp.getHeight();
+        int imgWidth  = bmp.getWidth();
+
+
+        //TRIM WIDTH - LEFT
+        int startWidth = 0;
+        for(int x = 0; x < imgWidth; x++) {
+            if (startWidth == 0) {
+                for (int y = 0; y < imgHeight; y++) {
+                    if (bmp.getPixel(x, y) != Color.TRANSPARENT) {
+                        startWidth = x;
+                        break;
+                    }
+                }
+            } else break;
+        }
+
+
+        //TRIM WIDTH - RIGHT
+        int endWidth  = 0;
+        for(int x = imgWidth - 1; x >= 0; x--) {
+            if (endWidth == 0) {
+                for (int y = 0; y < imgHeight; y++) {
+                    if (bmp.getPixel(x, y) != Color.TRANSPARENT) {
+                        endWidth = x;
+                        break;
+                    }
+                }
+            } else break;
+        }
+
+
+
+        //TRIM HEIGHT - TOP
+        int startHeight = 0;
+        for(int y = 0; y < imgHeight; y++) {
+            if (startHeight == 0) {
+                for (int x = 0; x < imgWidth; x++) {
+                    if (bmp.getPixel(x, y) != Color.TRANSPARENT) {
+                        startHeight = y;
+                        break;
+                    }
+                }
+            } else break;
+        }
+
+
+
+        //TRIM HEIGHT - BOTTOM
+        int endHeight = 0;
+        for(int y = imgHeight - 1; y >= 0; y--) {
+            if (endHeight == 0 ) {
+                for (int x = 0; x < imgWidth; x++) {
+                    if (bmp.getPixel(x, y) != Color.TRANSPARENT) {
+                        endHeight = y;
+                        break;
+                    }
+                }
+            } else break;
+        }
+
+
+        return Bitmap.createBitmap(
+                bmp,
+                startWidth,
+                startHeight,
+                endWidth - startWidth,
+                endHeight - startHeight
+        );
+
+    }
+}
